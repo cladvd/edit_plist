e2fe37ff2246197c67c803d2c7715f1c1669d6b1
Calò Davide <davide.calo@nttdata.com>
2020-02-11 22:33:58 +0100

    [master] - update README; define first version of script;

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# upload
+# Update manifest.plist for the ENTERPRISE release
+
+This script allows to update the properties used from Apple to perform a release in Enterprise Mode
+
+It can be used to **update** the following properties in the **manifest.plist**:
+
+- URL where is located the **.ipa** file
+- Bundle Version of the **.ipa** file
+
+
+# How To Use!
+After the creation and the signing of you **.ipa** file you can execute this script to update significant properties in the manifest.plist it using:
+
+```sh
+$ sh editPlist.sh  [remote_folder_where_your_file_will_be] [bundle_version] [file_name.ipa]
+```
+
+The script will create the release path with these properties and update the manifest that can be uploaded on the web page for Release in Enterprise Mode
--- script.sh
+++ script.sh
@@ -1,20 +1,15 @@
 #!/bin/bash   
 
-echo Ready to release... 
+#Script to update the manifest with data for the Enterprise release
 
-MANIFEST_FILE_NAME='manifest.plist' #Change this if your manifest has different name
-SOURCE_PATH=$1
-REMOTE_DESTINATION_FOLDER=$2
-BUNDLE_VERSION=$3
-REMOTE_FILE_NAME=$4
+MANIFEST_FILE_NAME='manifest.plist' #Change if you don't use the manifest in this repository
+REMOTE_DESTINATION_FOLDER=$1
+BUNDLE_VERSION=$2
+REMOTE_FILE_NAME=$3
 REMOTE_DESTINATION_PATH="$REMOTE_DESTINATION_FOLDER/$REMOTE_FILE_NAME"
-
-echo $REMOTE_DESTINATION_PATH
 
 /usr/libexec/Plistbuddy -c "Set :items:0:assets:0:url $REMOTE_DESTINATION_PATH" "$PWD/$MANIFEST_FILE_NAME"
 /usr/libexec/Plistbuddy -c "Set :items:0:metadata:bundle-version $BUNDLE_VERSION" "$PWD/$MANIFEST_FILE_NAME"
-
-
 
 exit 0
 
